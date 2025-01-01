c8a164e0b3f05fa1a4c7a1ebeac88d8856cad9bd
r.scherbakov <r.scherbakov@mail.ru>
2025-01-02 00:50:37 +0300

    gulp updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-const { src, dest,parallel, series, watch } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const fileInclude = require('gulp-file-include');
+const imagemin = require('gulp-imagemin')
 const browserSync = require('browser-sync').create()
 
 
@@ -10,6 +11,7 @@ function htmlInclude() {
 				prefix: '@@',
 				basepath: '@file',
 				context: {
+					scheme: 'light',
 					link: {
 						appStore: "https://apps.apple.com/ru/app/vmire/id1616533678"
 					}
@@ -17,12 +19,48 @@ function htmlInclude() {
 			})
 		)
 		.pipe(dest('./dist'))
-		//.pipe(browserSync.stream())
+	//.pipe(browserSync.stream())
+}
+
+function assets() {
+	return src(['src/assets/**/*.*'], { encoding: false })
+		.pipe(
+			imagemin(
+				[
+					imagemin.gifsicle({
+						interlaced: true,
+					}),
+					imagemin.mozjpeg({
+						quality: 90,
+						progressive: true,
+					}),
+					imagemin.optipng({
+						optimizationLevel: 5,
+					}),
+					imagemin.svgo({
+						plugins: [
+							{
+								removeViewBox: true,
+							},
+							{
+								cleanupIDs: false,
+							},
+						],
+					}),
+				]
+			)
+		)
+		.pipe(dest('dist/assets'))
+}
+
+function favicon() {
+	return src('src/*.ico')
+		.pipe(dest('dist'))
 }
 
 function build() {
-	return src(['!app/html/*.html', 'app/fonts/*.woff', 'app/fonts/*.woff2'], {
-		base: 'app',
+	return src(['!src/html/*.html', 'src/fonts/*.woff', 'src/fonts/*.woff2'], {
+		base: 'src',
 	}).pipe(dest('dist'))
 }
 
@@ -32,11 +70,13 @@ function cleanDist() {
 
 function watching() {
 	// watch(['src/**/*.html']).on('change', browserSync.reload)
-	watch(['src/html/*.html'], htmlInclude)
+	watch(['src/assets/**/*.*'], assets)
+	watch(['src/**/*.html'], htmlInclude)
 	watch(['src/*.html'], htmlInclude)
 }
 
 exports.watching = watching
+exports.assets = assets
 exports.cleanDist = cleanDist
 exports.htmlInclude = htmlInclude
 // exports.browsersync = browsersync
@@ -44,12 +84,14 @@ exports.htmlInclude = htmlInclude
 exports.build = series(
 	cleanDist,
 	htmlInclude,
+	assets,
 	build
 )
 
 
 exports.default = parallel(
 	htmlInclude,
+	assets,
 	//browsersync,
 	watching
 )
